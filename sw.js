c52b2a3c2e21b67e355cea0e7e28bd3e73476db3
todaynow0101-sys <todaynow0101@gmail.com>
2026-06-16 08:39:20 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'haru-v4';
+
+self.addEventListener('install', e => { self.skipWaiting(); });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
